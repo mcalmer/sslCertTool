d3074d43e8d552b5b078eea1667ebf98fe1892f4
Michael Calmer <mc@suse.de>
2016-06-27 16:42:50 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+#
+
+from distutils.core import setup
+
+setup(name = "sslCertTool",
+      version = "1.0.0",
+      description = "Tool to create SSL Certificates",
+      long_description = """\
+This Tool help to create SSL Certificates
+""",
+      author = 'Michael Calmer',
+      author_email = 'Michael.Calmer@suse.com',
+      url = 'https://github.com/mcalmer/sslCertTool',
+      py_modules = ["certTool"],
+      license = "GPLv2",
+      )
+
