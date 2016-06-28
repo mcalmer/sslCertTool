759702fb925e2b06733acf3f6ffd46156f048636
Michael Calmer <mc@suse.de>
2016-06-28 12:58:19 +0200

    options to constructor

--- certTool/certToolMain.py
+++ certTool/certToolMain.py
@@ -1,8 +1,26 @@
 
 class CertTool(object):
-    def __init__(self):
-        return
+    def __init__(self, opts):
+        self.genca = False
+        self.genserver = False
+        self.opts = opts
+        if opts.which == 'genca':
+            self._check_ca_opts()
+            self.genca = True
+        elif opts.which == 'genserver':
+            self._check_server_opts()
+            self.genserver = True
+        else:
+            raise CertToolException("invalid module")
 
-    def run(self, options):
+    def run(self):
         return 0
 
+    def _check_ca_opts(self):
+        self.opts.dir = normalizePath(self.opts.dir)
+        return 0
+
+    def _check_server_opts(self):
+        self.opts.dir = normalizePath(self.opts.dir)
+        return 0
+   
