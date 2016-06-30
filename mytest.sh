de6499edf3619de419ba9623ab293cf704ea1e6b
Michael Calmer <mc@suse.de>
2016-06-30 10:49:50 +0200

    call cert-tool from $PATH

--- mytest.sh
+++ mytest.sh
@@ -6,6 +6,8 @@ function error() {
     exit 1
 }
 
+PATH=$PATH:.
+
 TESTDIR1=testdir1
 TESTDIR2=testdir2
 TESTDIR3=testdir3
@@ -13,9 +15,9 @@ TESTDIR3=testdir3
 rm -rf $TESTDIR1/
 rm -rf $TESTDIR2/
 rm -rf $TESTDIR3/
-./cert-tool genca -d $TESTDIR1 -p hallo --common-name "SUSE CA" --state Bavaria --city Nuremberg --org "SUSE LLC" --email "suse@suse.com" --country "DE" -v --bits 4096 --cert-expiration 7000 --md sha512 || error
-./cert-tool genserver -d $TESTDIR1 -p hallo --hostname "lesch.suse.de" --state Bavaria --city Nuremberg --org "SUSE LLC" --email "suse@suse.com" --country "DE" -v --bits 4096 --cert-expiration 700 --md sha512 --cname "www.suse.de" || error
-./cert-tool genserver -d $TESTDIR1 -p hallo --hostname "lesch.suse.de" --state Bavaria --city Nuremberg --org "SUSE LLC" --email "suse@suse.com" --country "DE" -v --bits 4096 --cert-expiration 700 --md sha512 --cname "www.suse.de" --cname "w3.suse.de" || error
+cert-tool genca -d $TESTDIR1 -p hallo --common-name "SUSE CA" --state Bavaria --city Nuremberg --org "SUSE LLC" --email "suse@suse.com" --country "DE" -v --bits 4096 --cert-expiration 7000 --md sha512 || error
+cert-tool genserver -d $TESTDIR1 -p hallo --hostname "lesch.suse.de" --state Bavaria --city Nuremberg --org "SUSE LLC" --email "suse@suse.com" --country "DE" -v --bits 4096 --cert-expiration 700 --md sha512 --cname "www.suse.de" || error
+cert-tool genserver -d $TESTDIR1 -p hallo --hostname "lesch.suse.de" --state Bavaria --city Nuremberg --org "SUSE LLC" --email "suse@suse.com" --country "DE" -v --bits 4096 --cert-expiration 700 --md sha512 --cname "www.suse.de" --cname "w3.suse.de" || error
 test -f $TESTDIR1/CA-PRIVATE-SSL-KEY || error
 test -f $TESTDIR1/CA-TRUSTED-SSL-CERT || error
 test -f $TESTDIR1/ca-trusted-ssl-cert-1.0-1.noarch.rpm || error
@@ -30,17 +32,17 @@ test -f $TESTDIR1/lesch/ssl-servercert-key-pair-lesch-1.0-1.src.rpm || error
 
 mkdir $TESTDIR2
 cp $TESTDIR1/CA-* $TESTDIR2/
-./cert-tool genca -d $TESTDIR2 --rpm-only -v || error
+cert-tool genca -d $TESTDIR2 --rpm-only -v || error
 mkdir $TESTDIR2/lesch
 cp $TESTDIR1/lesch/ssl-servercert-key-pair-lesch-1.0-* $TESTDIR2/lesch/
 cp $TESTDIR1/lesch/server.{crt,key} $TESTDIR2/lesch/
-./cert-tool genserver -d $TESTDIR2 --rpm-only -v --hostname "lesch.suse.de" || error
-./cert-tool genserver -d $TESTDIR2 --rpm-only --hostname "" 2>&1 | grep "Require a machine name" >/dev/null || error
+cert-tool genserver -d $TESTDIR2 --rpm-only -v --hostname "lesch.suse.de" || error
+cert-tool genserver -d $TESTDIR2 --rpm-only --hostname "" 2>&1 | grep "Require a machine name" >/dev/null || error
 
 mkdir $TESTDIR3
 mkdir $TESTDIR3/lesch
-./cert-tool genca -d $TESTDIR3 --rpm-only 2>&1 | grep "does not exist" >/dev/null || error
-./cert-tool genserver -d $TESTDIR3 --rpm-only 2>&1 | grep "does not exist" >/dev/null || error
+cert-tool genca -d $TESTDIR3 --rpm-only 2>&1 | grep "does not exist" >/dev/null || error
+cert-tool genserver -d $TESTDIR3 --rpm-only 2>&1 | grep "does not exist" >/dev/null || error
 rm -rf $TESTDIR1/
 rm -rf $TESTDIR2/
 rm -rf $TESTDIR3/
